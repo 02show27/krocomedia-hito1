44cd2bedaaf7e62d2701b18af07c0ec266ac63fa
jhonnytitocastro-ship-it <jhonnytitocastro@gmail.com>
2026-08-09 22:23:51 -0400

    Aplicando mensajes de validaciones al formulario .js

--- script.js
+++ script.js
@@ -22,3 +22,95 @@ menuToggle.addEventListener("click", function () {
     menu.classList.toggle("active");
 
 });
+
+/* 09/08/2026 Mensajes de confirmacion */
+
+const formulario = document.querySelector("#contactForm");
+
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+const correo = document.querySelector("#correo");
+const telefono = document.querySelector("#telefono");
+
+const formMensaje = document.querySelector("#formMensaje");
+
+
+formulario.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+
+  const nombreValor = nombre.value.trim();
+  const apellidoValor = apellido.value.trim();
+  const correoValor = correo.value.trim();
+  const telefonoValor = telefono.value.trim();
+
+  formMensaje.textContent = "";
+
+  formMensaje.classList.remove("mostrar", "error", "exito");
+
+
+
+
+  if (nombreValor === "") {
+
+    formMensaje.textContent =
+      "Por favor, ingresa tu nombre.";
+
+    formMensaje.classList.add(
+      "mostrar",
+      "error"
+    );
+
+    return;
+  }
+
+  
+  if (apellidoValor === "") {
+
+    formMensaje.textContent =
+      "Por favor, ingresa tu apellido.";
+
+    formMensaje.classList.add(
+      "mostrar",
+      "error"
+    );
+
+    return;
+  }
+
+
+  if (!correoValor.includes("@")) {
+
+    formMensaje.textContent =
+      "Ingresa tu correo porfavor - El correo debe contener una arroba (@).";
+
+    formMensaje.classList.add(
+      "mostrar",
+      "error"
+    );
+
+    return;
+  }
+
+  if (!telefonoValor) {
+
+    formMensaje.textContent =
+      "Ingresa tu teléfono porfavor.";
+
+    formMensaje.classList.add(
+      "mostrar",
+      "error"
+    );
+
+    return;
+  }
+
+  formMensaje.textContent =
+    "¡Formulario enviado correctamente!";
+
+  formMensaje.classList.add(
+    "mostrar",
+    "exito"
+  );
+
+});
